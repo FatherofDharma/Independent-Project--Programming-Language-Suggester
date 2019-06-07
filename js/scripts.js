49feb9c74aee81d4fca6b10abd6f80706cf7b411
FatherofDharma <fatherofdharma@gmail.com>
2019-06-07 16:18:31 -0700

    add boolean evaluation to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
   $("#language").submit(function(event) {
-
-    var weather = $("input:radio[name=weather]:checked").val();
-    var drink = $("input:radio[name=drink]:checked").val();
-    var delight = $("input:radio[name=delight]:checked").val();
-    var color = $("input:radio[name=color]:checked").val();
-    var travel = $("input:radio[name=travel]:checked").val();
-
-    $("#language").hide();
-    $("#javascript").show();
-    $("#ruby").show();
-    $("#python").show();
     event.preventDefault();
-  });
+      var weather = $("input:radio[name=weather]:checked").val();
+      var drink = $("input:radio[name=drink]:checked").val();
+      var delight = $("input:radio[name=delight]:checked").val();
+      var color = $("input:radio[name=color]:checked").val();
+      var travel = $("input:radio[name=travel]:checked").val();
 
+      if (weather === "none" && drink === "none") {
+        $("#language").hide();
+        $("#javascript").show();
+      } else if (weather === "sunny" && drink === "lemonade" && delight === "none" && color === "none" && travel === "none" ) {
+        $("#language").hide();
+        $("#ruby").show();
+      } else if (weather === "cloudy" && drink === "coffee") {
+        $("#language").hide();
+        $("#python").show();
+      }  else if (weather === "rainy" && drink === "coffee") {
+        $("#language").hide();
+        $("#python").show();
+      } else if (weather === "snowy" && drink === "cocoa") {
+        $("#language").hide();
+        $("#python").show();
+      } else {
+        $("#language").hide();
+        $("#ruby").show();
+        $("#python").show();
+        $("#javascript").show();
+      };
   $("#resetForm").click(function() {
     document.getElementById("language").reset();
   });
+});
 });
