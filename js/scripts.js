cb76d4237e1c73edb61dc5e7bae114dc32ddb2ac
FatherofDharma <fatherofdharma@gmail.com>
2019-06-07 11:16:09 -0700

    fix an error from the console regarding the charset in <head>.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("#language").submit(function(event) {
-
+    // $(#language).hide();
     var weather = $("input:radio[name=weather]:checked").val();
     var drink = $("input:radio[name=drink]:checked").val();
     var delight = $("input:radio[name=delight]:checked").val();
