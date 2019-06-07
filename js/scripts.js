da08bf8b8d7347c3a3e9b0eaaa93d58b079c2b15
FatherofDharma <fatherofdharma@gmail.com>
2019-06-07 16:46:15 -0700

    clean up spaces and indentations. It will also add complete functionality to page.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
   $("#language").submit(function(event) {
-    event.preventDefault();
+
       var weather = $("input:radio[name=weather]:checked").val();
       var drink = $("input:radio[name=drink]:checked").val();
       var delight = $("input:radio[name=delight]:checked").val();
       var color = $("input:radio[name=color]:checked").val();
       var travel = $("input:radio[name=travel]:checked").val();
+      var three = ("You got all three!")
 
       if (weather === "none" && drink === "none") {
         $("#language").hide();
         $("#javascript").show();
-      } else if (weather === "sunny" && drink === "lemonade" && delight === "none" && color === "none" && travel === "none" ) {
+      } else if (weather === "sunny" && drink === "lemonade" ) {
         $("#language").hide();
         $("#ruby").show();
       } else if (weather === "cloudy" && drink === "coffee") {
@@ -28,8 +29,9 @@ $(document).ready(function(){
         $("#python").show();
         $("#javascript").show();
       };
+      event.preventDefault();
+  });
   $("#resetForm").click(function() {
     document.getElementById("language").reset();
   });
-});
 });
