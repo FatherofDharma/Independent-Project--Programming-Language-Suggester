eb2902542eb53f5fcb32a4cbf228349de61d153e
FatherofDharma <fatherofdharma@gmail.com>
2019-06-07 10:36:57 -0700

    add variable definitions and inital function structure to scripts.js.

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $("#language").submit(function(event) {
+
+    var weather = $("input:radio[name=weather]:checked").val();
+    var drink = $("input:radio[name=drink]:checked").val();
+    var delight = $("input:radio[name=delight]:checked").val();
+    var color = $("input:radio[name=color]:checked").val();
+    var travel = $("input:radio[name=travel]:checked").val();
+
+    event.preventDefault();
+  });
+});
