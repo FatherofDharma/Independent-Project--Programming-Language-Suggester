69df06d152bc9018b97d0c39e0ec9c5c861353d4
FatherofDharma <fatherofdharma@gmail.com>
2019-06-07 11:36:03 -0700

    add sections, id's, and css rules for the suggestions section.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
   $("#language").submit(function(event) {
-    // $(#language).hide();
+
     var weather = $("input:radio[name=weather]:checked").val();
     var drink = $("input:radio[name=drink]:checked").val();
     var delight = $("input:radio[name=delight]:checked").val();
     var color = $("input:radio[name=color]:checked").val();
     var travel = $("input:radio[name=travel]:checked").val();
+    
+    $("#language").hide();
+
 
     event.preventDefault();
   });
