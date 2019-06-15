a2c9ae749a0d036072a6b3a3e2b5ee74055c4090
FatherofDharma <fatherofdharma@gmail.com>
2019-06-15 15:59:12 -0700

    refactor business logic and separate it from interface logic. It will clean up spacing in html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,24 @@
-//Business Logic
+// Business Logic
+function showResult(score) {
+  // shows results based on score
+  if (score >= 16 && score <= 20) {
+    $('#javascript').show();
+  } else if (score >= 11 && score <= 15) {
+    $('#python').show();
+  } else if (score >= 6 && score <= 10) {
+    $('#ruby').show();
+  } else {
+    $('#three').show();
+    $('#ruby').show();
+    $('#python').show();
+    $('#javascript').show();
+  }
+}
 
-//Interface Logic Section
+// Interface Logic Section
 $(document).ready(function () {
   $('#language').submit(function (event) {
+    event.preventDefault();
     var weather = parseInt($('input:radio[name=weather]:checked').val());
     var drink = parseInt($('input:radio[name=drink]:checked').val());
     var delight = parseInt($('input:radio[name=delight]:checked').val());
@@ -10,23 +26,11 @@ $(document).ready(function () {
     var travel = parseInt($('input:radio[name=travel]:checked').val());
     var total = weather + drink + delight + color + travel;
 
-    // hide the form 'language' after form submission.
+    // hide the form 'language' after form submission
     $('#language').hide();
 
-    if (total >= 16 && total <= 20) {
-      $('#javascript').show();
-    } else if (total >= 11 && total <= 15) {
-      $('#python').show();
-    } else if (total >= 6 && total <= 10) {
-      $('#ruby').show();
-    } else {
-      $('#three').show();
-      $('#ruby').show();
-      $('#python').show();
-      $('#javascript').show();
-    }
-
-    event.preventDefault();
+    // show the result based on total
+    showResult(total);
   });
 
   $('#resetForm').click(function () {
